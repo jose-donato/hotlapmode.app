67c1d441141f99e587ff35c42e20b89a9b419751
jose-donato <zmcdonato@gmail.com>
2023-01-15 20:13:02 +0000

    feat: added button to download data

--- src/lib/ui/ComparisonQuali.svelte
+++ src/lib/ui/ComparisonQuali.svelte
@@ -33,6 +33,25 @@
 	});
 </script>
 
+<div class="absolute top-2 right-2 flex gap-2">
+	<button class="btn">
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width={1.5}
+			stroke="currentColor"
+			class="w-6 h-6"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+			/>
+		</svg>
+	</button>
+</div>
+
 <div class="grid grid-cols-2 w-full">
 	<Table columns={['Circuit', 'Driver1', 'Driver2', 'Difference']} rows={h2hQualiData} />
 	<Chart
