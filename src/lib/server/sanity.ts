f1dba230e5bd6c11ea4b9e597a6e4702d57c02fd
joaodunas <joaomariadonato@gmail.com>
2023-07-05 22:52:40 +0100

    added published

--- src/lib/server/sanity.ts
+++ src/lib/server/sanity.ts
@@ -8,9 +8,7 @@ const client = createClient({
 });
 
 export async function getThreads() {
-	const sanityThreads = await client.fetch(
-		`*[_type == "thread"] | order(_createdAt desc)`
-	);
+	const sanityThreads = await client.fetch(`*[_type == "thread" && status=="published"] | order(_createdAt desc)`);
 
 	return sanityThreads;
 }
@@ -18,7 +16,7 @@ export async function getThreads() {
 export async function getThread(id: string) {
 	const sanityThread = await client.fetch(`*[_type == "thread" && _id == "${id}"]`);
 	const recentThreads = await client.fetch(
-		`*[_type == "thread" && _id != "${id}"] | order(_createdAt desc) [0...5]`
+		`*[_type == "thread" && _id != "${id}" && status=="published"] | order(_createdAt desc) [0...5]`
 	);
 
 	sanityThread.recentThreads = recentThreads;
@@ -27,7 +25,9 @@ export async function getThread(id: string) {
 }
 
 export async function getFaq() {
-	const sanityFaq = await client.fetch(`*[_type == "faq"] | order(_createdAt desc)`);
+	const sanityFaq = await client.fetch(
+		`*[_type == "faq" && status=="published"] | order(_createdAt desc)`
+	);
 
 	return sanityFaq;
 }
